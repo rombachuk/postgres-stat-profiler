98f1853a4b1d393a70bc04cffe46015d92d247e5
Andrew Rombach <andrew.rombach@uk.ibm.com>
2024-01-03 10:21:40 +0000

    Corrections for pytest operation

--- tests/unit/test_api_auth.py
+++ tests/unit/test_api_auth.py
@@ -4,7 +4,7 @@ import uuid
 import os
 from unittest.mock import MagicMock
 from flask import request
-from postgres_stat_profiler.api_auth import api_auth
+from postgres_stat_profiler.api_auth.api_auth import api_auth
 
 os.environ['PG_STAT_PROFILER_LOGBASE'] = os.path.expanduser('~/Documents/GitHub/postgres_stat_profiler/postgres_stat_profiler/resources/log')
 logfilename = os.path.join(os.getenv(u'PG_STAT_PROFILER_LOGBASE'),u"pg-stat-profiler-unittest.log")
