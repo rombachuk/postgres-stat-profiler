b11c99e4332a23c7f51db387227923c066ee9957
Andrew Rombach <andrew.rombach@uk.ibm.com>
2024-01-31 16:54:11 +0000

    Minor fix to setup.py for factory pattern

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url='https://github.com/rombachuk/postgres_stat_profiler',
     entry_points={
       'console_scripts' : [
-          'pg_stat_profiler=postgres_stat_profiler.app:main'
+          'pg_stat_profiler=postgres_stat_profiler.wsgi:main'
       ]
     },
     packages=find_packages()
